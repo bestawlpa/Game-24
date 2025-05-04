d667d99b28f39ad03f8544b2e33c6582acd5730b
apiwat <bunderscorest@gmail.com>
2025-05-04 17:59:35 +0700

    ♻️ Test lint-staged

--- front-game24/app/routes/register.tsx
+++ front-game24/app/routes/register.tsx
@@ -15,6 +15,8 @@ export default function Register() {
   }
 
 
+
+
   return (
     <section className="bg-[#F2D2F4]">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
