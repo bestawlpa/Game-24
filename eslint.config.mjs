9e36b54d2d3089f770eaa4872d7b8f03e2e1313c
apiwat <bunderscorest@gmail.com>
2025-05-04 17:35:19 +0700

    ♻️ Test comment eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,34 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+// import js from "@eslint/js";
+// import globals from "globals";
+// import tseslint from "typescript-eslint";
+// import pluginReact from "eslint-plugin-react";
+// import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        sourceType: "module",
-        ecmaVersion: 2020,
-        ecmaFeatures: { jsx: true },
-        project: true,
-      },
-      globals: globals.browser,
-    },
-    plugins: {
-      js,
-      "@typescript-eslint": tseslint.plugin,
-      react: pluginReact,
-    },
-    rules: {
-      "react/react-in-jsx-scope": "off",
-    },
-  },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-]);
+// export default defineConfig([
+//   {
+//     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+//     languageOptions: {
+//       parser: tseslint.parser,
+//       parserOptions: {
+//         sourceType: "module",
+//         ecmaVersion: 2020,
+//         ecmaFeatures: { jsx: true },
+//         project: true,
+//       },
+//       globals: globals.browser,
+//     },
+//     plugins: {
+//       js,
+//       "@typescript-eslint": tseslint.plugin,
+//       react: pluginReact,
+//     },
+//     rules: {
+//       "react/react-in-jsx-scope": "off",
+//     },
+//   },
+//   tseslint.configs.recommended,
+//   pluginReact.configs.flat.recommended,
+// ]);
 
 
 
