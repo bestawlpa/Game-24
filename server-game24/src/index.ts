5c6727184c301d08a8e304326c71443f44eddb3f
apiwat <bunderscorest@gmail.com>
2025-05-04 19:55:33 +0700

    ✨ Add user route

--- server-game24/src/index.ts
+++ server-game24/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db';
+import user from './routes/userRoutes'
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 //     credentials: true,
 // }));
 
+app.use('/api', user)
 
 app.get('/', (req: Request, res: Response) => {
     res.status(200).send("My first server!");
