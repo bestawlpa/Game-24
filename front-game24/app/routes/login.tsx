8476ebe3966a64a62bb0335fe4ffffc6a98887fc
apiwat <bunderscorest@gmail.com>
2025-05-05 18:02:43 +0700

    ✨ Add func getUserProfile

--- front-game24/app/routes/login.tsx
+++ front-game24/app/routes/login.tsx
@@ -27,12 +27,32 @@ export default function Login() {
         const errorData = await response.json();
         throw new Error(errorData.message || "Registration failed");
       }
+      getUserProfile()
       alert('Login successful')
     } catch (error) {
       const err = error as Error;
       alert("Login failed:" + err.message);
     }
   };
+
+  const getUserProfile = async () => {
+    try {
+      const response = await fetch(`http://localhost:3088/api/profile`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+      });
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || "Failed to fetch user profile");
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      const err = error as Error;
+      alert(err.message);
+    }
+  }
 
 
   return (
@@ -55,7 +75,7 @@ export default function Login() {
                 </label>
                 <input
                   type="text"
-                  name="text"
+                  name="username"
                   id="text"
                   className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="username"
