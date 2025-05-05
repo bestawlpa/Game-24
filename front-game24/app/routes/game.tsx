5ecc9380a9dd7d93259dda68c2168351c74b53a0
apiwat <bunderscorest@gmail.com>
2025-05-05 18:03:50 +0700

    ✨ Add new page: /game

--- front-game24/app/routes/game.tsx
+++ front-game24/app/routes/game.tsx
@@ -1,15 +1,38 @@
-export default function Game() {
+import { useState } from 'react';
 
-    const handle = () => {
-        console.log('test01');
-        console.log('test02');
-        console.log('test03');
+export default function Game() {
+    const [numbers, setNumbers] = useState<number[]>([]);
+
+    const fetchNumber = async () => {
+        try {
+            const response = await fetch('http://localhost:3088/api/generate-numbers');
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(errorData.message);
+            }
+            const data = await response.json();
+
+            const spt = data.number.toString().split('').map(Number);
+            console.log('data', spt);
+            setNumbers(spt);
+        } catch (error) {
+            const err = error as Error;
+            console.log(err);
+        }
+    }
+
+    const handleClick = (n: Number) => {
+        console.log(n);
     }
 
     return (
         <div>
-            <h1>game</h1>
-            <h2>24</h2>
+            {numbers.map((num, index) => (
+                <button key={index} onClick={() => handleClick(num)}>
+                    {num}
+                </button>
+            ))}
+            <button onClick={fetchNumber}>start</button>
         </div>
     )
 }
