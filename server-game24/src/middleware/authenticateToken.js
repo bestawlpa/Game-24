39de827965e0b61b0c72992d6468671bda2b124f
apiwat <bunderscorest@gmail.com>
2025-05-06 14:49:54 +0700

    🚨 Complie authentoken

--- server-game24/src/middleware/authenticateToken.js
+++ server-game24/src/middleware/authenticateToken.js
@@ -5,9 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const SECRET_KEY = process.env.SECRET_KEY;
-// interface AuthenticatedRequest extends Request {
-//     user?: IUser;
-// }
 const authenticateToken = (req, res, next) => {
     const token = req.cookies.jwtToken;
     if (!token) {
